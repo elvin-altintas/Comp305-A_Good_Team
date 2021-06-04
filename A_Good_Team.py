3b1b04a548bd995a004ed436ee9159c2b143f961
zeynepdundarr <40126925+zeynepdundarr@users.noreply.github.com>
2021-06-04 23:09:39 +0300

    Add files via upload
    Test Results Added

--- A_Good_Team.py
+++ A_Good_Team.py
@@ -1,89 +1,118 @@
-## A Good Team - Room 28 Term Project
+	## A Good Team - Room 28 Term Project
 ## Elvin Altıntaş, İrem Zeynep Dündar, Kutluhan Palalıoğlu, Simay Özdemir
 
 ## Neighbor number of each spot on the board can be calculated via neighbor_count(i, j, arr)
 ## Cornes, edges and middle spots are evaluated differently
+def read_sample_output():
+  sample_output = {}
+  file1 = open("small-testdata.out", "r+")
+  txt = file1.read()
+  output = txt.split("\n")
+
+  for k in range(len(output) - 1):
+    out = output[k].split(":")[1]
+    sample_output[k] = int(out[1:len(out)])
+  return sample_output
+
+
+def read_sample_input():
+  sample_test_cases = {}
+  file1 = open("small-testdata.in", "r+")
+  txt = file1.read()
+  txt_split = txt.split("\n")
+  total_test_case_no = int(txt_split[0])  # txt[0:len(txt[0])]
+  test_case_data = txt_split[1:len(txt_split)]
+
+  i = 0
+  for k in range(total_test_case_no):
+    rc = test_case_data[i].split()
+    r, c = int(rc[0]), int(rc[1])
+    sample_test_cases[k] = test_case_data[i + 1: i + 1 + r]
+    i = r + i + 1
+  return sample_test_cases
+
+
 def neighbor_count(i, j, arr):
-    neighbor = 0
+  neighbor = 0
 
-    if j == 0:
+  if j == 0:
 
-        if i == 0:
+      if i == 0:
 
-            if (i, j + 1) in assigned:  # right
-                neighbor += 1
-            if (i + 1, j) in assigned:  # down
-                neighbor += 1
+          if (i, j + 1) in assigned:  # right
+              neighbor += 1
+          if (i + 1, j) in assigned:  # down
+              neighbor += 1
 
-        elif i == len(arr) - 1:  # (n-1,1),(n-2,0)
+      elif i == len(arr) - 1:  # (n-1,1),(n-2,0)
 
-            if (i - 1, j) in assigned:  # up
-                neighbor += 1
-            if (i, j + 1) in assigned:  # right
-                neighbor += 1
+          if (i - 1, j) in assigned:  # up
+              neighbor += 1
+          if (i, j + 1) in assigned:  # right
+              neighbor += 1
 
-        else:  # (i-1, j), (i+1,j), (i,j+1)
+      else:  # (i-1, j), (i+1,j), (i,j+1)
 
-            if (i - 1, j) in assigned:  # up
-                neighbor += 1
-            if (i + 1, j) in assigned:  # down
-                neighbor += 1
-            if (i, j + 1) in assigned:  # right
-                neighbor += 1
-    elif j == len(arr[0]) - 1:
+          if (i - 1, j) in assigned:  # up
+              neighbor += 1
+          if (i + 1, j) in assigned:  # down
+              neighbor += 1
+          if (i, j + 1) in assigned:  # right
+              neighbor += 1
+  elif j == len(arr[0]) - 1:
 
-        if i == 0:
+      if i == 0:
 
-            if (i, j - 1) in assigned:  # left
-                neighbor += 1
+          if (i, j - 1) in assigned:  # left
+              neighbor += 1
 
-            if (i + 1, j) in assigned:  # down
-                neighbor += 1
+          if (i + 1, j) in assigned:  # down
+              neighbor += 1
 
 
-        elif i == len(arr) - 1:
+      elif i == len(arr) - 1:
 
-            if (i - 1, j) in assigned:  # up
-                neighbor += 1
-            if (i, j - 1) in assigned:  # left
-                neighbor += 1
+          if (i - 1, j) in assigned:  # up
+              neighbor += 1
+          if (i, j - 1) in assigned:  # left
+              neighbor += 1
 
-        else:
+      else:
 
-            if (i - 1, j) in assigned:  # up
-                neighbor += 1
-            if (i, j - 1) in assigned:  # left
-                neighbor += 1
-            if (i + 1, j) in assigned:  # down
-                neighbor += 1
-    elif i == 0:
+          if (i - 1, j) in assigned:  # up
+              neighbor += 1
+          if (i, j - 1) in assigned:  # left
+              neighbor += 1
+          if (i + 1, j) in assigned:  # down
+              neighbor += 1
+  elif i == 0:
 
-        if (i, j + 1) in assigned:  # right
-            neighbor += 1
-        if (i, j - 1) in assigned:  # left
-            neighbor += 1
-        if (i + 1, j) in assigned:  # down
-            neighbor += 1
-    elif i == len(arr) - 1:
+      if (i, j + 1) in assigned:  # right
+          neighbor += 1
+      if (i, j - 1) in assigned:  # left
+          neighbor += 1
+      if (i + 1, j) in assigned:  # down
+          neighbor += 1
+  elif i == len(arr) - 1:
 
-        if (i, j + 1) in assigned:  # right
-            neighbor += 1
-        if (i, j - 1) in assigned:  # left
-            neighbor += 1
-        if (i - 1, j) in assigned:  # up
-            neighbor += 1
-    else:
+      if (i, j + 1) in assigned:  # right
+          neighbor += 1
+      if (i, j - 1) in assigned:  # left
+          neighbor += 1
+      if (i - 1, j) in assigned:  # up
+          neighbor += 1
+  else:
 
-        if (i, j + 1) in assigned:  # right
-            neighbor += 1
-        if (i, j - 1) in assigned:  # left
-            neighbor += 1
-        if (i - 1, j) in assigned:  # up
-            neighbor += 1
-        if (i + 1, j) in assigned:  # down
-            neighbor += 1
+      if (i, j + 1) in assigned:  # right
+          neighbor += 1
+      if (i, j - 1) in assigned:  # left
+          neighbor += 1
+      if (i - 1, j) in assigned:  # up
+          neighbor += 1
+      if (i + 1, j) in assigned:  # down
+          neighbor += 1
 
-    return neighbor
+  return neighbor
 
 ## Takes the input: test case number, height and width of the board, assignments of spots
 def read_inputs():
@@ -102,7 +131,7 @@ assigned = {}  # spot list
 available = {}  # available for spots
 
 ## This function checks the pre-assigned spots to determine which checkerboard format is the most suitable
-## Pre-assigned spots belonging to a specific version increases the score of that version, 
+## Pre-assigned spots belonging to a specific version increases the score of that version,
 ## unavailable spots belonging to a specific version decreases the score of that version.
 ## Returns the most suitable chekerboard version
 def format_board(arr):
@@ -199,23 +228,22 @@ if __name__ == "__main__":
     # ###?#...
 
     # extra test case 1
-# 2
-# 7
-# 5
-# # ???.
-# ??  # ?#
-# ?????
-# .?..?
-# ?  # ?#?
-# ????.
-# .?  # .?
-# 6 5
-# ??#?#
-# ?????
-# .?..?
-# ?#?#?
-# ????.
-# .?#.?
+    # 2
+    # 7 5
+    # # ???.
+    # ??  # ?#
+    # ?????
+    # .?..?
+    # ?  # ?#?
+    # ????.
+    # .?  # .?
+    # 6 5
+    # ??#?#
+    # ?????
+    # .?..?
+    # ?#?#?
+    # ????.
+    # .?#.?
 
     # extra test case 2
     # 2
@@ -254,13 +282,30 @@ if __name__ == "__main__":
     #         "???",
     #         "?##",
     #         "?##",]
-    # print(a_good_team(test))
+    #print(a_good_team(test))
 
-    test_case = int(input())
-    test_list=list()
+    # test_case = int(input())
+    # test_list=list()
+    #
+    # for i in range(test_case):
+    #    test_list.append(read_inputs())
+    #
+    #
+    # for test in test_list:
+    #    print("Score: ",a_good_team(test))
 
-    for i in range(test_case):
-       test_list.append(read_inputs())
-    print("Outputs:\n")
-    for test in test_list:
-        print("Score ",a_good_team(test))
+    # default test cases
+    corr = 0
+    sample_test_cases = read_sample_input()
+    sample_test_outputs = read_sample_output()
+
+    for t in range(len(sample_test_cases.keys())):
+      print()
+      print("Test case t:", t)
+      print("Algorithm Score:", a_good_team(sample_test_cases[t]), " | ",
+            "Real Score:", sample_test_outputs[t])
+      if sample_test_outputs[t] == a_good_team(sample_test_cases[t]):
+        corr += 1
+      print()
+
+    print("Accuracy of the algorithm: ", corr / 100)
